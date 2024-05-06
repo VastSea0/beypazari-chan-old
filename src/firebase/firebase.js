41d2da31b011544ada14f78629ae0ae21629ef25
Egehan KAHRAMAN <144556903+VastSea0@users.noreply.github.com>
2024-05-06 21:20:05 +0300

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,15 +4,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAgjb9v0LS5AtjIKr_6ejWkvPVdvubIHXY",
-    authDomain: "opensodaproject.firebaseapp.com",
-    projectId: "opensodaproject",
-    storageBucket: "opensodaproject.appspot.com",
-    messagingSenderId: "364099706420",
-    appId: "1:364099706420:web:6d39db2bff04a3af6ff461",
-    measurementId: "G-E7ZWYC9NJV"
-  };
+ 
   
   
 // Firebase'i başlatın
