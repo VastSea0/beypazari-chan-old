c294e18b3b762a6141ea33204f8d1daa2622bba5
Egehan KAHRAMAN <144556903+VastSea0@users.noreply.github.com>
2024-05-08 22:13:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './components/Home';
 import Study from './components/Study';
 import KelimeCalis from './components/KelimeCalis';
+import SocialPage from './components/Social';
 
 function App() {
   return (
     <Router>
       <div className="App">
-          
         <Routes>
           <Route path="/" element={<HomePage />} />   
           <Route path="/calis" element={<Study />} />   
           <Route path='/calis/kelime' element={<KelimeCalis />} />
+          <Route path='/sosyal/' element={<SocialPage></SocialPage>} />
         </Routes>
       </div>
     </Router>
